342dfd337e62713b94e9d80ab5c27f7fddb39f5d
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-11-17 02:39:58 +1000

    Verify TL canonicalisation in processing

--- Tests/Canonicalisation/testCanonicaliser.py
+++ Tests/Canonicalisation/testCanonicaliser.py
@@ -12,7 +12,14 @@ class testCanonicaliser(baseTest):
         output_dir = tempfile.gettempdir()
         foo = Canonicaliser(zaru, output_dir)
         foo.process()
+        # Copy of New Vision starline in dictionary should not be TL-canonical
+        targ = [item for item in foo.dictionary.lines if 'New Vision' in item][0]
+        self.assertTrue('E4248DA-7' in targ, "New Vision starline TL-canonicalised ahead of time")
+
         foo.write()
+        # Verify switch
+        targ = [item for item in foo.dictionary.lines if 'New Vision' in item][0]
+        self.assertTrue('E4248DA-6' in targ, "New Vision starline not TL-canonicalised")
 
 
 if __name__ == '__main__':
