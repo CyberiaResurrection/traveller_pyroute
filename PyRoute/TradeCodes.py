0d87de9bb0e01e69a61ced1104aef21d71bc6943
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-11-18 23:01:05 +1000

    Handle two-word dieback codes

--- PyRoute/TradeCodes.py
+++ PyRoute/TradeCodes.py
@@ -162,6 +162,7 @@ class TradeCodes(object):
         while i < num_codes - 1:
             i += 1
             raw = raw_codes[i]
+            nextcode = None if i == num_codes - 1 else raw_codes[i + 1]
             # Filter duplicates
             if raw in codes:
                 continue
@@ -178,7 +179,7 @@ class TradeCodes(object):
             if 2 < len(raw) and (raw.startswith('[]') or raw.startswith('()')):
                 raw = raw[2:]
             if raw.startswith('Di('):
-                if not raw.endswith(')') and i < num_codes - 1:
+                if not raw.endswith(')') and i < num_codes - 1:  # We've found a two-word sophont code
                     next = raw_codes[i + 1]
                     if next.endswith(')'):
                         combo = raw + ' ' + next
@@ -258,9 +259,8 @@ class TradeCodes(object):
                     continue
 
             if i < num_codes - 1:
-                next = raw_codes[i + 1]
-                if next.endswith(')') or next.endswith(']'):
-                    combo = raw + ' ' + next
+                if nextcode.endswith(')') or nextcode.endswith(']'):
+                    combo = raw + ' ' + nextcode
                     codes.append(combo)
                     raw_codes[i + 1] = ''
                 continue
