0e101b2bb259d4c83e8fa5a439525e76ec9b54ee
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-02-13 18:21:07 +1000

    Clean up invalid escape sequence warnings

--- PyRoute/Inputs/ParseStarInput.py
+++ PyRoute/Inputs/ParseStarInput.py
@@ -19,7 +19,7 @@ from PyRoute.TradeCodes import TradeCodes
 
 
 class ParseStarInput:
-    regex = """
+    regex = r"""
 ^((?:0[1-9]|[1-2]\d|3[0-2])(?:0[1-9]|40|[1-3]\d)) +
 (.{15,}) +
 ([A-HXYa-hxy][0-9A-Fa-f]\w\w[0-9A-Fa-f][0-9A-Xa-x][0-9A-Ja-j]-\w|\?\?\?\?\?\?\?-\?|[A-HXYa-hxy\?][0-9A-Fa-f\?][\w\?]{2,2}[0-9A-Fa-f\?][0-9A-Xa-x\?][0-9A-Ja-j\?]-[\w\?]) +
