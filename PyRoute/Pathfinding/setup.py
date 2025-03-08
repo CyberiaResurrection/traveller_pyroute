be50414a621771a6669aa758d21bb9f351f5acfc
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-03-08 21:54:04 +1000

    Wrap unordered_map for use in bidir_numpy

--- PyRoute/Pathfinding/setup.py
+++ PyRoute/Pathfinding/setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(
         ['astar_numpy.py', 'single_source_dijkstra_core.py', 'ApproximateShortestPathForestUnified.py',
-         'minmaxheap.pyx', 'bidir_numpy.py'],
+         'minmaxheap.pyx', 'bidir_numpy.py', "unordered_map.pyx"],
         annotate=False
     ),
     include_dirs=[numpy.get_include()]
