14481a40bdbe1794cf8b084bf55cff69dd3fd1f2
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-03-10 23:59:00 +1000

    Pass upbound to bidirectional pathfinding

--- Tests/Pathfinding/testTradeCalculation.py
+++ Tests/Pathfinding/testTradeCalculation.py
@@ -58,3 +58,25 @@ class testTradeCalculation(baseTest):
         galaxy.trade.calculate_components()
 
         galaxy.trade.calculate_routes()
+
+    def test_bidirectional_duplicate_blowup(self):
+        sourcefile = self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup/Core.sec')
+
+        args = self._make_args()
+        args.route_btn = 8
+        args.max_jump = 4
+        args.routes = "trade"
+        readparms = ReadSectorOptions(sectors=[sourcefile], pop_code=args.pop_code, ru_calc=args.ru_calc,
+                                      route_reuse=args.route_reuse, trade_choice=args.routes, route_btn=args.route_btn,
+                                      mp_threads=args.mp_threads, debug_flag=args.debug_flag, fix_pop=False,
+                                      deep_space={}, map_type=args.map_type)
+
+        galaxy = Galaxy(min_btn=15, max_jump=4)
+        galaxy.read_sectors(readparms)
+        galaxy.output_path = args.output
+
+        galaxy.generate_routes()
+        galaxy.set_borders(args.borders, args.ally_match)
+        galaxy.trade.calculate_components()
+
+        galaxy.trade.calculate_routes()
