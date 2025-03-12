849c03877f5fb0bc6444271ea505da7c13714d0f
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-03-12 15:58:52 +1000

    Add self-parent checks in path construction

--- PyRoute/Pathfinding/bidir_numpy.py
+++ PyRoute/Pathfinding/bidir_numpy.py
@@ -287,6 +287,7 @@ def bidir_build_path(explored_fwd: umap[cython.int, cython.int], explored_rev: u
         assert node not in path, "Node " + str(node) + " duplicated in discovered path, " + str(path)
         path.append(node)
         node = explored_fwd[node]
+        assert node != explored_fwd[node], "Node " + str(node) + " is own forward ancestor"
     path.reverse()
 
     node = explored_rev[smalldex]
@@ -294,4 +295,5 @@ def bidir_build_path(explored_fwd: umap[cython.int, cython.int], explored_rev: u
         assert node not in path, "Node " + str(node) + " duplicated in discovered path, " + str(path)
         path.append(node)
         node = explored_rev[node]
+        assert node != explored_rev[node], "Node " + str(node) + " is own reverse ancestor"
     return path
