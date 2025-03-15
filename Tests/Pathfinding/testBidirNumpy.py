bc85ca7ff91424292f356c481a591dce12054e74
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-03-15 13:51:06 +1000

    Add test for fourth bidir duplicate blowup

--- Tests/Pathfinding/testBidirNumpy.py
+++ Tests/Pathfinding/testBidirNumpy.py
@@ -77,3 +77,25 @@ class testBidirNumpy(baseTest):
         galaxy.trade.calculate_components()
 
         galaxy.trade.calculate_routes()
+
+    def testBidirectionalBlowup4(self):
+        sourcefile = self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_4/Solomani Rim.sec')
+
+        args = self._make_args()
+        args.route_btn = 8
+        args.max_jump = 4
+        args.routes = "trade"
+        readparms = ReadSectorOptions(sectors=[sourcefile], pop_code=args.pop_code, ru_calc=args.ru_calc,
+                                      route_reuse=args.route_reuse, trade_choice=args.routes, route_btn=args.route_btn,
+                                      mp_threads=args.mp_threads, debug_flag=args.debug_flag, fix_pop=False,
+                                      deep_space={}, map_type=args.map_type)
+
+        galaxy = Galaxy(min_btn=15, max_jump=4)
+        galaxy.read_sectors(readparms)
+        galaxy.output_path = args.output
+
+        galaxy.generate_routes()
+        galaxy.set_borders(args.borders, args.ally_match)
+        galaxy.trade.calculate_components()
+
+        galaxy.trade.calculate_routes()
