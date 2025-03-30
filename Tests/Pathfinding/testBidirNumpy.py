12393c6b7dde123ed48077afb5b1fb4543ad213d
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-03-30 23:24:06 +1000

    Add test for ninth bidir duplicate blowup

--- Tests/Pathfinding/testBidirNumpy.py
+++ Tests/Pathfinding/testBidirNumpy.py
@@ -215,3 +215,31 @@ class testBidirNumpy(baseTest):
         galaxy.trade.calculate_components()
 
         galaxy.trade.calculate_routes()
+
+    def testBidirectionalBlowup9(self):
+        sources = [
+            self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_9/Core.sec'),
+            self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_9/Fornast.sec'),
+            self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_9/Gateway.sec'),
+            self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_9/Ley Sector.sec'),
+            self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_9/Lishun.sec'),
+        ]
+
+        args = self._make_args()
+        args.route_btn = 8
+        args.max_jump = 4
+        args.routes = "trade"
+        readparms = ReadSectorOptions(sectors=sources, pop_code=args.pop_code, ru_calc=args.ru_calc,
+                                      route_reuse=args.route_reuse, trade_choice=args.routes, route_btn=args.route_btn,
+                                      mp_threads=args.mp_threads, debug_flag=args.debug_flag, fix_pop=False,
+                                      deep_space={}, map_type=args.map_type)
+
+        galaxy = Galaxy(min_btn=15, max_jump=4)
+        galaxy.read_sectors(readparms)
+        galaxy.output_path = args.output
+
+        galaxy.generate_routes()
+        galaxy.set_borders(args.borders, args.ally_match)
+        galaxy.trade.calculate_components()
+
+        galaxy.trade.calculate_routes()
