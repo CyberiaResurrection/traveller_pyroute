705d5b25185df0a63cffeab807bd5e66e902ad4f
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-03-31 17:39:30 +1000

    Drop obsolete test

--- Tests/Pathfinding/testBidirNumpy.py
+++ Tests/Pathfinding/testBidirNumpy.py
@@ -118,36 +118,6 @@ class testBidirNumpy(baseTest):
 
         galaxy.trade.calculate_routes()
 
-    def testBidirectionalBlowup5a(self):
-        sourcefile = self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_5/Zarushagar.sec')
-
-        args = self._make_args()
-        args.route_btn = 8
-        args.max_jump = 4
-        args.routes = "trade"
-        readparms = ReadSectorOptions(sectors=[sourcefile], pop_code=args.pop_code, ru_calc=args.ru_calc,
-                                      route_reuse=args.route_reuse, trade_choice=args.routes, route_btn=args.route_btn,
-                                      mp_threads=args.mp_threads, debug_flag=args.debug_flag, fix_pop=False,
-                                      deep_space={}, map_type=args.map_type)
-
-        galaxy = Galaxy(min_btn=15, max_jump=4)
-        galaxy.read_sectors(readparms)
-        galaxy.output_path = args.output
-
-        galaxy.generate_routes()
-        galaxy.set_borders(args.borders, args.ally_match)
-        galaxy.trade.calculate_components()
-
-        source = max(galaxy.star_mapping.values(), key=lambda item: item.wtn)
-        source.is_landmark = True
-        galaxy.trade.shortest_path_tree = ApproximateShortestPathForestUnified(source.index, galaxy.stars,
-                                                                               galaxy.trade.epsilon)
-
-        raw_route, _ = bidir_path_numpy(galaxy.trade.star_graph, 39, 47,
-                                        galaxy.trade.shortest_path_tree.lower_bound_bulk)
-
-        self.assertIsNotNone(raw_route, "Candidates should return a route")
-
     def testBidirectionalBlowup6(self):
         sourcefile = self.unpack_filename('DeltaFiles/bidirectional_duplicate_blowup_6/Zarushagar.sec')
 
