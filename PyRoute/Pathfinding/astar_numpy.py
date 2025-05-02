474adb4d92a34ded6d512386e55ddf9cc9078f93
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-05-02 12:26:52 +1000

    Rename dist in astar_t struct

--- PyRoute/Pathfinding/astar_numpy.py
+++ PyRoute/Pathfinding/astar_numpy.py
@@ -137,12 +137,12 @@ def astar_numpy_core(G_succ: cython.list[cython.tuple[cnp.ndarray[cython.int], c
     # The queue stores priority, cost to reach, node, and parent.
     # Comparisons are handled by astar_t directly.
     queue: MinMaxHeap[astar_t] = MinMaxHeap[astar_t]()
-    queue.insert({'augment': potentials_view[source], 'dist': 0.0, 'curnode': source, 'parent': ROOT_NODE})
+    queue.insert({'augment': potentials_view[source], 'cost': 0.0, 'curnode': source, 'parent': ROOT_NODE})
 
     while 0 < queue.size():
         # Pop the smallest item from queue.
         result = queue.popmin()
-        dist = result.dist
+        dist = result.cost
         curnode = result.curnode
         parent = result.parent
         node_counter += 1
@@ -203,7 +203,7 @@ def astar_numpy_core(G_succ: cython.list[cython.tuple[cnp.ndarray[cython.int], c
             if aug_wt > upbound:
                 continue
             distances_view[act_nod] = act_wt
-            queue.insert({'augment': aug_wt, 'dist': act_wt, 'curnode': act_nod, 'parent': curnode})
+            queue.insert({'augment': aug_wt, 'cost': act_wt, 'curnode': act_nod, 'parent': curnode})
             counter += 1
             if target == act_nod:
                 upbound = aug_wt
